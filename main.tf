b5b530b8ac55cc6f0275872eb3bc0e6ee4d09800
gastonfreire3xm <gaston.freire@3xmgroup.com>
2022-01-07 09:56:20 -0300

    Point 1

--- main.tf
+++ main.tf
@@ -27,10 +27,10 @@ provider "aws" {
 resource "random_pet" "sg" {}
 
 resource "aws_instance" "web" {
-  ami = "ami-09e67e426f25ce0d7"
+  ami           = "ami-09e67e426f25ce0d7"
   instance_type = "t2.micro"
   tags = {
-    project = "training"
+    project   = "training"
     createdBy = "gastonFreire"
   }
   vpc_security_group_ids = [aws_security_group.web-sg.id]
@@ -45,9 +45,9 @@ resource "aws_instance" "web" {
 resource "aws_security_group" "web-sg" {
   name = "${random_pet.sg.id}-sg"
   ingress {
-    from_port = 8080
-    to_port = 8080
-    protocol = "tcp"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
